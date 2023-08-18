1413cbce15113b995b63cc875ab707cd0e1765dc
natasakurandic <natasakurandic@gmail.com>
2023-08-18 23:42:42 +0200

    Add task 10

--- src/main/java/p18_08_2023/Vezbanje10.java
+++ src/main/java/p18_08_2023/Vezbanje10.java
@@ -1,5 +1,7 @@
 package p18_08_2023;
 
+import java.util.Scanner;
+
 public class Vezbanje10 {
 //    (Za vezbanje)
 //    Napisati program koji stampa 20 pozicija.
@@ -13,6 +15,22 @@ public class Vezbanje10 {
 //    Objasnjenje: stampamo _ od 5 do 10 u ostalim situacijama stampamo *
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Uneti broj A od 1 do 10");
+            int a = s.nextInt();
+        System.out.println("Uneti broj B od 1 do 10");
+            int b = s.nextInt();
+
+        for (int i = 1; i < 21; i++) {
+            if (i >= a && i <= b) {
+                System.out.print("_ ");
+            } else {
+                System.out.print("* ");
+
+            }
+        }
+
 
     }
 
